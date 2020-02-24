4e766478cc43dcb97a9c7038cac103e8a0ab3714
Rodhatul Vivia Firdaus Azhari <54561826+viviaazh@users.noreply.github.com>
2020-02-24 18:57:51 +0700

    Membuat halaman utama, registrasi, dan profil

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -3,12 +3,43 @@ package id.ac.polinema.intentexercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static id.ac.polinema.intentexercise.RegisterActivity.ABOUT_KEY;
+import static id.ac.polinema.intentexercise.RegisterActivity.CPASSWORD_KEY;
+import static id.ac.polinema.intentexercise.RegisterActivity.EMAIL_KEY;
+import static id.ac.polinema.intentexercise.RegisterActivity.FULLNAME_KEY;
+import static id.ac.polinema.intentexercise.RegisterActivity.HOMEPAGE_KEY;
+import static id.ac.polinema.intentexercise.RegisterActivity.PASSWORD_KEY;
 
 public class ProfileActivity extends AppCompatActivity {
+    private TextView fullnameText;
+    private TextView emailText;
+    private TextView passwordText;
+    private TextView cpasswordText;
+    private TextView homepageText;
+    private TextView aboutText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        fullnameText = findViewById(R.id.text_fullname);
+        emailText = findViewById(R.id.text_email);
+        passwordText = findViewById(R.id.text_password);
+        cpasswordText = findViewById(R.id.text_confirm_password);
+        homepageText = findViewById(R.id.text_homepage);
+        aboutText = findViewById(R.id.text_about);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            fullnameText.setText(extras.getString(FULLNAME_KEY));
+            emailText.setText(extras.getString(EMAIL_KEY));
+            passwordText.setText(extras.getString(PASSWORD_KEY));
+            cpasswordText.setText(extras.getString(CPASSWORD_KEY));
+            homepageText.setText(extras.getString(HOMEPAGE_KEY));
+            aboutText.setText(extras.getString(ABOUT_KEY));
+        }
+
     }
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -2,13 +2,57 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    public static final String FULLNAME_KEY = "fullname";
+    public static final String EMAIL_KEY = "email";
+    public static final String PASSWORD_KEY = "password";
+    public static final String CPASSWORD_KEY = "confirmpassword";
+    public static final String HOMEPAGE_KEY = "homepage";
+    public static final String ABOUT_KEY = "about";
+
+    private EditText fullnameInput;
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText cpasswordInput;
+    private EditText homepageInput;
+    private EditText aboutInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        fullnameInput = findViewById(R.id.text_fullname);
+        emailInput = findViewById(R.id.text_email);
+        passwordInput = findViewById(R.id.text_password);
+        cpasswordInput = findViewById(R.id.text_confirm_password);
+        homepageInput = findViewById(R.id.text_homepage);
+        aboutInput = findViewById(R.id.text_about);
+
+    }
+
+    public void handleOk(View view) {
+        String fullname = fullnameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String cpassword = cpasswordInput.getText().toString();
+        String homepage = homepageInput.getText().toString();
+        String about = aboutInput.getText().toString();
+
+        Intent intent = new Intent(this, ProfileActivity.class);
+        intent.putExtra(FULLNAME_KEY, fullname);
+        intent.putExtra(EMAIL_KEY, email);
+        intent.putExtra(PASSWORD_KEY, password);
+        intent.putExtra(CPASSWORD_KEY, cpassword);
+        intent.putExtra(HOMEPAGE_KEY, homepage);
+        intent.putExtra(ABOUT_KEY, about);
+        startActivity(intent);
+    }
+
+    public void handleChangePict(View view) {
     }
 }
